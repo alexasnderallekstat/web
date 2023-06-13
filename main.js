03b0c95ea8a34267a243b7e3a1db14ecb3f490af
Alexsander <135511367+allekstat@users.noreply.github.com>
2023-06-13 06:23:03 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function flo(x) {
+	return Math.floor(x);
+}
+function fil(x, y=2) {
+	return '0'.repeat(y -x.toString().length) +x;
+}
+var t = flo(new DocumentTimeline().currentTime /1e3),
+    s = flo(flo(t %60)),
+    m = flo(flo(t %(60 **2)) /60),
+    h = flo(flo(t %(60 **3)) /(60 **2)),
+    decorridos = `${h > 0 ? fil(h) +':' : ''}${h > 0 || m > 0 ? fil(m) +':' : ''}${h > 0 || m > 0 ? fil(s) : fil(s) +'s'} decorridos.`;
