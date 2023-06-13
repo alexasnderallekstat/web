6e04112a2a65c9a41db2cc105e549b8ef79a4b53
Alexsander <135511367+allekstat@users.noreply.github.com>
2023-06-13 07:33:08 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -12,3 +12,34 @@ function canvas_calibrar_tamanho(resolucao=100) {
 	return false;
 }
 canvas_calibrar_tamanho();
+
+class Nave {
+	constructor(specs={}) {
+		const padrao = {x: 0, y: 0, a: 0, v: 1, tamanho: 1, vel_rotacao: 1, vel_disparo: 1, dano: 1};
+		specs = {...padrao, ...specs};
+		for(let s in specs) {
+			this[s] = specs[s];
+		}
+	}
+	desenhar(c) {
+		const {x, y} = this;
+		c.beginPath();
+		c.fillStyle = 'black';
+		c.moveTo(x, y);
+	}
+}
+
+const matematica = {
+	tan(x) { return Math.tan(x) },
+	atan(x) { return Math.atan(x) },
+	pi: return Math.PI,
+	arredondar(x) { return 0 +fix(x +atan(tan(-x *pi -pi /(2 **50))) /pi) },
+	chao(x) { return 0 +fix(x +atan(tan(-x *pi +pi /2 -pi /(2 **50))) /pi -0.5) },
+	teto(x) { return 0 +fix(x +atan(tan(-x *pi +pi /2 +pi /(2 **50))) /pi +0.5) },
+	resto(x, l=1) { return a -chao(a /l) *l },
+	decimal(x) {  return 0 +fix(x -chao(x)) },
+	raiz(x, r=2) { return 0 +fix(x **(1 /r)) },
+	soma(x=0, y=0) { x = fix(x); y = fix(y); return fix(x +y) },
+	multi(x=0, y=1) { x = fix(x); y = fix(y); return fix(x *y) },
+	fix(x) { if (typeof x !== 'number' || x +'' === 'NaN' || x +'' === 'Infinity') { return null } return (x).toFixed(6) *1 }
+}
