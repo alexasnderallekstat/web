d4e79b87fd7f46a4cc24afe2452d7cfc9fc8aac0
Alexsander <135511367+allekstat@users.noreply.github.com>
2023-06-13 06:02:55 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const c = canvas.getContext('2d');
-function canvas_calibrar_tamanho() {
-	const w = 16 * 40,
-	      h =  9 * 40;
+function canvas_calibrar_tamanho(resolucao=100) {
+	canvas.style.height = document.querySelector('article').clientHeight + 'px';
+	const h = resolucao /100 *(canvas.style.height.slice(0, -2) *1),
+	      w = Math.floor((16 /9) *h);
 	canvas.width = w;
 	canvas.height = h;
 	if(canvas.width === w && canvas.height === h) return true;
