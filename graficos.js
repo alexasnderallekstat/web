7fe93f9c28af5f1421a87ae575e1242e06b0e38e
Alexsander <135511367+allekstat@users.noreply.github.com>
2023-06-13 05:03:27 -0300

    funcao calibrar checa resultado

--- graficos.js
+++ graficos.js
@@ -2,7 +2,10 @@ const canvas = document.getElementsByTagName('canvas')[0];
 const c = canvas.getContext('2d');
 canvas.style.backgroundColor = 'red';
 function canvas_calibrar_tamanho() {
-	canvas.width = 16*40;	canvas.heigth = 9*40;
-	
-	return true;
+	const w = 16 * 40,
+	      h =  9 * 40;
+	canvas.width = w;
+	canvas.height = h;
+	if(canvas.width === w && canvas.height === h) return true;
+	return false;
 }
