3c3ab25ca74b654e6c242fe70b76c398ab53a9a1
Alexsander <135511367+allekstat@users.noreply.github.com>
2023-06-13 05:31:59 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const c = canvas.getContext('2d');
-canvas.style.backgroundColor = 'red';
 function canvas_calibrar_tamanho() {
 	const w = 16 * 40,
 	      h =  9 * 40;
